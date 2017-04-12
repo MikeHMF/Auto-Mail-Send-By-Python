d107e4efd132eb91750e3ca052321f531c81da5a
WangQingye <329103586@qq.com>
2017-04-12 23:34:14 +0800

    歌曲

--- info.py
+++ info.py
@@ -90,6 +90,6 @@ content1= "亲爱的<b>李琳垚</b>:" + "<br>" +\
       "    " + xingzuo[2:] + "<br><br>" + \
       "<b>再送上笑话一则：</b>" + "<br>" + \
       "" + xiaohua + "<br><br>" + \
-      "<b>今日歌曲一首</b>：<a href=http://music.163.com/#/song?id=25731632>三年三天- 安与骑兵</a><br><br>" + \
+      "<b>今日歌曲一首</b>：<a href=http://music.163.com/#/song?id=37196629>不为谁而作的歌- 林俊杰</a><br><br>" + \
       "<b>最后也是最重要的</b>：" + "<br>"
 
